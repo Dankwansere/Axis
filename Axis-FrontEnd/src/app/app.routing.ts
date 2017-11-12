3c6dbc423ae8233f389155248293106d305217f9
Eric <ericsarpong@hotmail.com>
2017-11-11 21:42:48 -0500

    added canActivate Route guard to Payroll and profile screen

--- Axis-FrontEnd/src/app/app.routing.ts
+++ Axis-FrontEnd/src/app/app.routing.ts
@@ -6,11 +6,13 @@ import {ProfileComponent} from './profile/profile.component';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home.component'
 
+import {AuthGuard} from './auth-guard.service'
+
 export const routing = RouterModule.forRoot([
     {path: '', component: HomeComponent},
-    {path: 'payroll', component: PayrollComponent},
+    {path: 'payroll', component: PayrollComponent, canActivate:[AuthGuard]},
     {path: 'career', component: CareerComponent},
-    {path: 'profile', component: ProfileComponent},
+    {path: 'profile', component: ProfileComponent, canActivate:[AuthGuard]},
     {path: 'login', component: LoginComponent}
 
 ])
