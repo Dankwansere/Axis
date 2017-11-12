24ec23a830ecb361659139a3877814a05906d04d
Eric <ericsarpong@hotmail.com>
2017-11-11 19:46:23 -0500

    added login path to routing table

--- Axis-FrontEnd/src/app/app.routing.ts
+++ Axis-FrontEnd/src/app/app.routing.ts
@@ -3,11 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {PayrollComponent} from './payroll/payroll.component';
 import {CareerComponent} from './career/career.component';
 import {ProfileComponent} from './profile/profile.component';
+import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home.component'
 
 export const routing = RouterModule.forRoot([
     {path: '', component: HomeComponent},
     {path: 'payroll', component: PayrollComponent},
     {path: 'career', component: CareerComponent},
-    {path: 'profile', component: ProfileComponent}
+    {path: 'profile', component: ProfileComponent},
+    {path: 'login', component: LoginComponent}
+
 ])
