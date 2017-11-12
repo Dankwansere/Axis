580a634599f1796f900f7be2e848c996c6498d2c
Eric <ericsarpong@hotmail.com>
2017-11-11 21:45:49 -0500

    updated logic for login with dummy data

--- Axis-FrontEnd/src/app/login/login.service.ts
+++ Axis-FrontEnd/src/app/login/login.service.ts
@@ -4,14 +4,16 @@ import {ActiveUser} from './ActiveUser';
 
 @Injectable()
 export class LoginService {
-    isLoggedIn : Boolean = false;
+    isLoggedIn : boolean = false;
     user:ActiveUser;
 
-    public Login(userName:string, password:string){
+    public Login(userName:string, password:string): boolean{
         this.user = new ActiveUser(userName, password);
 
         if(this.user.username == "Eric" && this.user.password == "123"){
-            
+            this.isLoggedIn = true;
         }
+
+        return  this.isLoggedIn;
     }
 }
