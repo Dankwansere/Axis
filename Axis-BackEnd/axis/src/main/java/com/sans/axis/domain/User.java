4d50d2315a92c638a632e8f96eb10406676be1ec
Eric <ericsarpong@hotmail.com>
2017-12-06 15:51:27 -0500

    User entity class

---
+++ Axis-BackEnd/axis/src/main/java/com/sans/axis/domain/User.java
@@ -0,0 +1,74 @@
+package com.sans.axis.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email_add")
+	private String emailAdd;
+	
+	@Column(name="password")
+	private String passWord;
+	
+	public User() {
+		
+	}
+	
+	public User(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailAdd() {
+		return emailAdd;
+	}
+	public void setEmailAdd(String emailAdd) {
+		this.emailAdd = emailAdd;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+	
+	
+	
+	
+	
+}
