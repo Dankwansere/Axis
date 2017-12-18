71a2e3f97a4acccec22355f57caafb7d1e24aefd
Eric <ericsarpong@hotmail.com>
2017-12-17 23:24:55 -0500

    Implemented logic using observables to validate username after 4 characters have been inputted, display corresponding icon if username is acceptable or not

--- Axis-FrontEnd/src/app/login/register.component.ts
+++ Axis-FrontEnd/src/app/login/register.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
 import {LoginService} from './login.service';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component ({
     selector : 'register',
@@ -9,8 +12,14 @@ import {LoginService} from './login.service';
 
 export class RegisterComponent {
 
+    isUsernameValid: boolean;
+    displayIcon: boolean;
+
+    private checkMarkIconPath:string = "./app/assets/icons/checkmark.png";
+    private xMarkIconPath:string = "./app/assets/icons/xmark.png";
+    imageUrl:string;
     
-    userForm = new FormGroup ({
+   @Input() userForm = new FormGroup ({
         userName: new FormControl(),
         firstName: new FormControl(),
         lastName: new FormControl(),
@@ -22,22 +31,49 @@ export class RegisterComponent {
     });
 
     constructor(private loginService : LoginService){
-
+        
     }
 
 
     public register() {
 
         console.log(this.userForm.value);
-        /*console.log(this.userForm.controls["firstName"].value);
-        console.log(this.userForm.controls["lastName"].value);
-        console.log(this.userForm.controls["emailAdd"].value);
-        console.log(this.userForm.controls["passWord"].value);
-        console.log(this.userForm.controls["gender"].value); */
+       
         let userFormJson = JSON.stringify(this.userForm.value);
 
-        this.loginService.registerPostRequest(userFormJson).subscribe();
+        this.loginService.registerPostRequest(userFormJson).subscribe(data => console.log(data));
 
+    }
+
+    private validateUsername(value){
+        this.displayIcon = true;
+
+        if(value._body == "true"){
+            this.imageUrl = this.xMarkIconPath;
+        }
+        else if(value._body == "false"){
+            this.imageUrl = this.checkMarkIconPath;
+        }
+    }
+
+    private resetUsernameIcon(data) : any{
+        if(data.length == 0){
+            this.displayIcon = false;
+            this.imageUrl = "";
+        }
+    }
+
+    ngOnInit(){
+
+        this.userForm.controls["userName"].valueChanges.filter(text => this.resetUsernameIcon(text)).subscribe();
+
+        this.userForm.controls["userName"].valueChanges
+        .filter(text => text.length >= 4)
+        .debounceTime(400)
+        .distinctUntilChanged()
+        .subscribe(data => this.loginService
+            .validateUserNameGetRequest(this.userForm.controls["userName"].value)
+            .subscribe(value => this.validateUsername(value)));
     }
     
 }
