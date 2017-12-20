759bfa767cc62e942ca4fea79f1334ec05e63176
Eric <ericsarpong@hotmail.com>
2017-12-19 23:14:56 -0500

    updated login method to include condition for displaying loading spinner

--- Axis-FrontEnd/src/app/login/login.component.ts
+++ Axis-FrontEnd/src/app/login/login.component.ts
@@ -2,13 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {LoginService} from './login.service';
 import{FormGroup, FormControl} from '@angular/forms';
 
+
 @Component ({
     selector: 'login',
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styleUrls: ['logReg.component.css']
 })
 
 export class LoginComponent{
 
+    isLoading:boolean = false;
+   
     loginForm = new FormGroup ({
         userName: new FormControl(),
         passWord: new FormControl()
@@ -21,12 +25,30 @@ export class LoginComponent{
     }
 
     public login(){
-       this._loginService.loginPostRequest(this.loginForm.value).subscribe(data => console.log(data));
-        this.isUserLoggedIn = true;
+        this.isLoading = true;
+        this._loginService.loginPostRequest(this.loginForm.value)
+          .subscribe(data => {
+            this.verifyResponse(data)
+            this.isLoading = false;
+        });
+    }
+
+    public verifyResponse(data){
+        let loginResponse:string = data._body;
+
+        if(loginResponse == "true") {
+            this._loginService.isLoggedIn = true;
+            this.isUserLoggedIn = this._loginService.isLoggedIn;
+            
+        }
+        else {
+            this.isUserLoggedIn = false;
+        }
+        
+        console.log("from response: " + this.isUserLoggedIn);
     }
 
     ngOnInit(){
         this.isUserLoggedIn = this._loginService.isLoggedIn;
     }
-
 }
