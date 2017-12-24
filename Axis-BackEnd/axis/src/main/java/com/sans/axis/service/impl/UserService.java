9ab5e4210fd16a4c897826c7310f3061458b60d7
Eric <ericsarpong@hotmail.com>
2017-12-23 21:27:09 -0500

    removed data layer logic from service.

--- Axis-BackEnd/axis/src/main/java/com/sans/axis/service/impl/UserService.java
+++ Axis-BackEnd/axis/src/main/java/com/sans/axis/service/impl/UserService.java
@@ -1,83 +1,39 @@
 package com.sans.axis.service.impl;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Service;
-
 import com.sans.axis.domain.User;
-import com.sans.axis.domain.repository.IUserRepository;
+import com.sans.axis.domain.repository.IUserCustomRepository;
 import com.sans.axis.service.IUserService;
 
 @Service
 public class UserService implements IUserService  {
 
 	@Autowired
-	private IUserRepository userRepository;
-	
-	@PersistenceContext
-	private EntityManager em;
+	private IUserCustomRepository customUserRepository;
 	
 	
 	@Override
 	public User getUser(String userName, String passWord) {
-		User user;
-		Query query = em.createQuery("SELECT u from User u WHERE userName = :userName AND passWord = :passWord");
-		query.setParameter("userName", userName);
-		query.setParameter("passWord", passWord);
-		
-		try {
-			user = (User) query.getSingleResult();
+		User user = customUserRepository.getUser(userName, passWord);	
+		if(user != null) {
 			return user;
-		} 
-		catch (Exception e) {
-			
-			System.out.println("Unable to find user: " + e.getMessage());
+		}
+		else {
+			System.out.println("Unable to find user: " + userName);
 			return null;
 		}
-			
 	}
 	
 	@Override
 	public boolean validateUserName(String username) {
-		Query query = em.createQuery("SELECT userName from User u WHERE userName = :username");
-		query.setParameter("username", username);
-		
-		try {
-			List<User> result = query.getResultList();
-			
-			if(result.size() == 0) {
-				return false;
-			}
-			else {
-				return true;
-			}
-		}
-		catch (Exception ex) {
-			System.out.println("Something went wrong searching for user: " + ex.getMessage());
-			return false;
-		}
+		return customUserRepository.validateUserName(username);
 	}
 	
 	
 	@Override
 	public boolean createUser(User user) {
-		
-		try {
-			userRepository.save(user);
-		
-			return true;
-		}
-		catch(Exception ex) {
-			System.out.println("Error: " + ex.getMessage());
-			return false;
-		}
+		return customUserRepository.createUser(user);
 	}
 
 }
