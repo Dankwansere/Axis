7e1d064fc986fceb747645e07f0b264e6b7a8712
Eric <ericsarpong@hotmail.com>
2018-01-27 18:23:28 -0500

    Parsing json response string to user object

--- Axis-FrontEnd/src/app/login/login.component.ts
+++ Axis-FrontEnd/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from './login.service';
 import{FormGroup, FormControl} from '@angular/forms';
+import {User} from '../model/user';
+import {ParseJsonObject} from '../utilities/parseJsonObject';
 
 
 @Component ({
@@ -11,18 +13,16 @@ import{FormGroup, FormControl} from '@angular/forms';
 
 export class LoginComponent{
 
-    isLoading:boolean = false;
-   
+    _user: User;
+    isLoading: boolean = false;
     loginForm = new FormGroup ({
         userName: new FormControl(),
         passWord: new FormControl()
     });
 
-    isUserLoggedIn : boolean;
+    isUserLoggedIn: boolean;
 
-    constructor(private _loginService:LoginService){
-        
-    }
+    constructor(private _loginService:LoginService){}
 
     public login(){
         this.isLoading = true;
@@ -34,18 +34,10 @@ export class LoginComponent{
     }
 
     public verifyResponse(data){
-        let loginResponse:string = data._body;
-
-        if(loginResponse == "true") {
-            this._loginService.isLoggedIn = true;
-            this.isUserLoggedIn = this._loginService.isLoggedIn;
-            
+        this._user = ParseJsonObject.convertJsonToUserObject(JSON.parse(data._body));
+        if(this._user != null){
+            this._user.setLoggedIn(true);
         }
-        else {
-            this.isUserLoggedIn = false;
-        }
-        
-        console.log("from response: " + this.isUserLoggedIn);
     }
 
     ngOnInit(){
