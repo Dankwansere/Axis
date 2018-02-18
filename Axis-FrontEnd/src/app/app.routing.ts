8869c033e43d350826c0320a43e0d9ec92fba4ab
Eric <ericsarpong@hotmail.com>
2018-02-17 20:53:29 -0500

    Timesheet routing component added, and minor changes to others

--- Axis-FrontEnd/src/app/app.routing.ts
+++ Axis-FrontEnd/src/app/app.routing.ts
@@ -7,6 +7,7 @@ import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
 import {LogRegComponent} from './login/logReg.component';
 import {RegisterComponent} from './login/register.component';
+import {Timesheet} from './timesheet/timesheet.component';
 
 
 import {AuthGuard} from './auth-guard.service';
@@ -14,8 +15,10 @@ import {AuthGuard} from './auth-guard.service';
 export const routing = RouterModule.forRoot([
     {path: '', component: HomeComponent},
     {path: 'payroll', component: PayrollComponent, canActivate:[AuthGuard]},
-    {path: 'career', component: CareerComponent},
     {path: 'profile', component: ProfileComponent, canActivate:[AuthGuard]},
+    {path: 'timesheet', component: Timesheet, canActivate:[AuthGuard]},
+    {path: 'career', component: CareerComponent},
+    {path: 'addNewUser', component: RegisterComponent},
     {path: 'logReg', component: LogRegComponent,
         children: [
         {path: 'login', component: LoginComponent},
