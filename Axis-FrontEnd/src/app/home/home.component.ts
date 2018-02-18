ef4063ea91520385201e4d47ae2dcef49060be45
Eric <ericsarpong@hotmail.com>
2018-02-17 20:55:47 -0500

    implemented onInit method to retrieve username of logged in user to be displayd on home screen welcome message

--- Axis-FrontEnd/src/app/home/home.component.ts
+++ Axis-FrontEnd/src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Authentication } from '../commons/authentication';
 
 @Component({
     selector: 'home',
     templateUrl: 'home.component.html'
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    userName: string;
+
+    public ngOnInit() {
+        if(Authentication.isUserSessionActive) {
+            this.userName = Authentication.retrieveSessionUserObject().userName;
+        }
+    }
 }
+
+
+
 
