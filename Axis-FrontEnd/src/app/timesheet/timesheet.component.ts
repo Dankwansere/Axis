ee8f9cf41c790e0d20e6e0ae531ff4a734a9f402
Eric <ericsarpong@hotmail.com>
2018-02-24 00:03:26 -0500

    Implemented onInit to interpolate the user first and lastname by pulling the active user from session.

--- Axis-FrontEnd/src/app/timesheet/timesheet.component.ts
+++ Axis-FrontEnd/src/app/timesheet/timesheet.component.ts
@@ -1,11 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl} from '@angular/forms';
+import { Authentication } from '../commons/authentication';
+import { User } from '../model/user';
+
 
 @Component({
     selector: 'timeSheet',
     templateUrl: 'timesheet.component.html'
 })
 
-export class Timesheet {
+export class Timesheet implements OnInit {
+    TimesheetUser: User;
+    FullName: String;
+    YearList: String [] = new Array(5); 
+    today = new Date() ;
 
+    
+    
+    
+    
+    public ngOnInit() {
+        this.TimesheetUser = Authentication.retrieveSessionUserObject();
+        this.FullName = this.TimesheetUser.firstName + " " + this.TimesheetUser.lastName;
+        this.YearList[0] = this.today.getFullYear().toString();
+        console.log(this.today.getDay() - 2);
+        console.log("YEAR 0: ", this.YearList[0] );
+
+    }
 }
