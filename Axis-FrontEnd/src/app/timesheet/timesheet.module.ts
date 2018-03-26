bed72170998cba9b9c535d8e995ed9778585baa9
Eric <ericsarpong@hotmail.com>
2018-03-25 21:14:03 -0400

    added browser and forms and reactive forms module, and also Timesheet Service and Timesheet resolver

--- Axis-FrontEnd/src/app/timesheet/timesheet.module.ts
+++ Axis-FrontEnd/src/app/timesheet/timesheet.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Timesheet} from './timesheet.component';
-
+import { BrowserModule } from '@angular/platform-browser';
+import {TimeSheetService} from '../services/timesheet.service';
+import {TimesheetResolver} from '../services/timesheetResolver.service'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
     declarations: [Timesheet],
-    imports:  [],
+    imports:  [BrowserModule, FormsModule, ReactiveFormsModule],
     exports: [],
-    providers: []
+    providers: [TimeSheetService, TimesheetResolver]
    
 })
 
