e4c120a3309cf4b8b9d7bf9a5c26216fd7f53046
Eric <ericsarpong@hotmail.com>
2018-03-25 21:03:05 -0400

    import path updates

--- Axis-FrontEnd/src/app/login/login.component.ts
+++ Axis-FrontEnd/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {LoginService} from './login.service';
+import {LoginService} from '../services/login.service';
 import {FormGroup, FormControl} from '@angular/forms';
 import {User} from '../model/user';
 import {Authentication} from '../commons/authentication';
