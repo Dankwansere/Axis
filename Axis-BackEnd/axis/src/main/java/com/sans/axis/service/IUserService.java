fd73b7b8b246b0940377962e07b6bfd9f8e47aad
Eric <ericsarpong@hotmail.com>
2018-04-07 18:20:27 -0400

    Adding new getUserProjects method to interface

--- Axis-BackEnd/axis/src/main/java/com/sans/axis/service/IUserService.java
+++ Axis-BackEnd/axis/src/main/java/com/sans/axis/service/IUserService.java
@@ -1,11 +1,16 @@
 package com.sans.axis.service;
 
+import java.util.ArrayList;
+
+import com.sans.axis.domain.GenericControlList;
 import com.sans.axis.domain.User;
 
 public interface IUserService {
 	
 	
 	public User getUser(String userName, String password);
+	
+	public ArrayList<GenericControlList> getUserProjects();
 	
 	public boolean createUser(User user);
 	
