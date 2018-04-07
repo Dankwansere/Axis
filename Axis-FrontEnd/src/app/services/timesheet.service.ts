e0aa62169b8b72698ca814923c38b0f6cb2a2555
Eric <ericsarpong@hotmail.com>
2018-04-07 18:30:31 -0400

    New service method to submit timesheet to backend

--- Axis-FrontEnd/src/app/services/timesheet.service.ts
+++ Axis-FrontEnd/src/app/services/timesheet.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {RequestOptions, Headers} from '@angular/http';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { BaseApiService } from './baseApiService';
+import {FormArray} from '@angular/forms';
 
 @Injectable()
 export class TimeSheetService {
@@ -10,6 +11,10 @@ export class TimeSheetService {
 
     public getUserProjectList() {
         return this.apiService.getRequest('user/timesheet');
+    }
+
+    public submitTimesheet(timesheetForm: FormArray) {
+        return this.apiService.postRequest
     }
 
 }
