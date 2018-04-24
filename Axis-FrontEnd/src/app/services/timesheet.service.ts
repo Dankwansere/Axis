eda679813b1453bc159899fce885921c70035a5a
Eric <ericsarpong@hotmail.com>
2018-04-24 16:34:33 -0400

    changing "timesheetForm" parameter type from FormArray to any, for generic purposes

--- Axis-FrontEnd/src/app/services/timesheet.service.ts
+++ Axis-FrontEnd/src/app/services/timesheet.service.ts
@@ -9,12 +9,15 @@ export class TimeSheetService {
 
     constructor(private _http: HttpClient,private apiService: BaseApiService) {}
 
+    timesheetBaseUrl = "timesheet/";
+
     public getUserProjectList() {
         return this.apiService.getRequest('user/timesheet');
     }
 
-    public submitTimesheet(timesheetForm: FormArray) {
-        return this.apiService.postRequest
+    public submitTimesheet(timesheetForm: any) {
+        let url = this.timesheetBaseUrl + "submitTimesheetForm";
+        return this.apiService.postRequest(url, timesheetForm);
     }
 
 }
