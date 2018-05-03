55c349f07ef722e96a778e0a81c84e2ff2841cc3
Eric <ericsarpong@hotmail.com>
2018-05-03 19:56:07 -0400

    external css

--- Axis-FrontEnd/src/app/login/logReg.component.ts
+++ Axis-FrontEnd/src/app/login/logReg.component.ts
@@ -3,7 +3,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component ({
     selector: 'logReg',
-    templateUrl: 'logReg.component.html'
+    templateUrl: 'logReg.component.html',
+    styleUrls: ['logReg.component.css']
 })
 
 export class LogRegComponent {
