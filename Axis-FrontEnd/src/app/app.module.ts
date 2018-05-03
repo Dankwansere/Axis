75c142fdc133f13a599aff16566d188eb902609e
Eric <ericsarpong@hotmail.com>
2018-05-03 19:51:26 -0400

    added web interceptor module

--- Axis-FrontEnd/src/app/app.module.ts
+++ Axis-FrontEnd/src/app/app.module.ts
@@ -8,7 +8,7 @@ import {PayrollModule} from './payroll/payroll.module';
 import {ProfileModule} from './profile/profile.module';
 import {LoginModule} from './login/login.module';
 import {TimesheetModule} from './timesheet/timesheet.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {HomeComponent} from './home/home.component';
@@ -17,6 +17,8 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {routing} from './app.routing';
 import {AuthGuard} from './guards/auth-guard.service';
 import {CanDeactivateGuard} from './guards/deactivate-guard.service'
+
+import {WebInterceptor} from './interceptors/webinterceptor';
 
 
 
@@ -29,7 +31,7 @@ import {CanDeactivateGuard} from './guards/deactivate-guard.service'
     BrowserModule, FormsModule, HttpClientModule, CareerModule,
     PayrollModule, ProfileModule, LoginModule, TimesheetModule, routing
   ],
-  providers: [AuthGuard, CanDeactivateGuard],
+  providers: [AuthGuard, CanDeactivateGuard, { provide: HTTP_INTERCEPTORS, useClass: WebInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
