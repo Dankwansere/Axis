24c70b83dd5393a4a651b0de7f2fb7dc1d91ea7b
Eric <ericsarpong@hotmail.com>
2018-05-03 19:58:41 -0400

    import path update

--- Axis-FrontEnd/src/app/services/login.service.ts
+++ Axis-FrontEnd/src/app/services/login.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import { BaseApiService } from './baseApiService';
+import { BaseApiService } from './baseApi.service';
 
 
 @Injectable()
--- Axis-FrontEnd/src/app/services/timesheet.service.ts
+++ Axis-FrontEnd/src/app/services/timesheet.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {RequestOptions, Headers} from '@angular/http';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import { BaseApiService } from './baseApiService';
+import { BaseApiService } from './baseApi.service';
 import {FormArray} from '@angular/forms';
 
 @Injectable()
