73fc847dd47856eb8549e62c6c60acf83562e1f7
Eric <ericsarpong@hotmail.com>
2018-05-23 21:59:25 -0400

    Implementing angular custom modal pop up window

--- Axis-FrontEnd/src/app/login/login.component.ts
+++ Axis-FrontEnd/src/app/login/login.component.ts
@@ -6,7 +6,9 @@ import {Authentication} from '../commons/authentication';
 import {JsonConvert} from 'json2typescript';
 import { Router } from '@angular/router';
 
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA, MatDialogRef, MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component ({
     selector: 'login',
@@ -23,12 +25,20 @@ export class LoginComponent implements OnInit {
         passWord: new FormControl()
     });
 
+
     displayLoginErrorMsg: boolean;
     isUserLoggedIn: boolean;
     userLoginErrorMessage: string;
 
+    //private passwdIcon: string = require('../../app/assets/icons/passwd.png');
+
     constructor(private _loginService: LoginService, private router: Router,
-         @Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<LoginComponent>) {}
+         @Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<LoginComponent>,
+         iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+            iconRegistry.addSvgIcon(
+                'thumbs-up',
+                sanitizer.bypassSecurityTrustResourceUrl('../../app/assets/icons/passwd.svg'));
+         }
 
     public login() {
         this.isLoading = true;
@@ -44,7 +54,6 @@ export class LoginComponent implements OnInit {
     }
 
     public verifyResponse(data) {
-
         const jsonConvert: JsonConvert = new JsonConvert();
         try {
             this._user = jsonConvert.deserialize(data, User);
