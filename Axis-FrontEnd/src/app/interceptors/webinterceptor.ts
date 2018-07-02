565db82af42b910935c7a3b05cf30d18eb728878
Eric <ericsarpong@hotmail.com>
2018-07-02 19:25:38 -0400

    injecting session stored token into every subsequent http requests

--- Axis-FrontEnd/src/app/interceptors/webinterceptor.ts
+++ Axis-FrontEnd/src/app/interceptors/webinterceptor.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core';
 import {HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-
+import {User} from '../model/user';
+import {Authentication} from '../commons/authentication';
 @Injectable()
 export class WebInterceptor implements HttpInterceptor {
 
+    private user: User;
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let interceptedRequest: HttpRequest<any>;
+        this.user = Authentication.retrieveSessionUserObject();
+        let jwtToken: string;
 
-       // console.log("request url: ", request.url + " ", request.urlWithParams);
+        if (this.user != null || this.user != undefined) {
+            jwtToken = this.user.token;
+        }
 
-        const request2 = request.clone({setHeaders: {
-            Authorization: 'Bearer'}
-        });
-       // console.log("tt headers: ", request2.headers);
+        if (jwtToken) {
+             interceptedRequest = request.clone({setHeaders: {
+                Authorization: this.user.token}
+            });
+            console.log('Intercepted request: ', interceptedRequest)
+            return next.handle(interceptedRequest);
+        } else {
+            return next.handle(request);
+        }
 
-        return next.handle(request2);
     }
 }
